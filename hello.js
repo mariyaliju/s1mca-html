1eed7ed6c9ca5cc04837c860078393d9910e2e40
mariyaliju <mariyaliju2002@gmail.com>
2024-11-04 14:48:11 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,8 @@
+.Hello world using javascript
+<html>
+<body>
+<script language="javascript"type="text/javascript">
+document.write("HELLO WORLD")
+</script>
+</body>
+</html>
---
+++ mariya.js
@@ -0,0 +1,7 @@
+<html>
+<body>
+<script language="javascript"type="text/javascript">
+document.write("MARIYA")
+</script>
+</body>
+</html>
